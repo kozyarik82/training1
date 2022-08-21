4f2872cb2b9712b2368645f1ddc068fcfd4aa388
kozyar <kozyarik@gmail.com>
2022-08-21 23:52:52 +0200

    есть результат

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("самостоятельная работа!");
+        System.out.println("я все сделала сама, ура!");
     }
 }
